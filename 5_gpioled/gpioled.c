d1b1749dbc8444c9e73ba4dfe3e3aee42d909342
troye <192567797+troyeeee673@users.noreply.github.com>
2026-06-27 22:45:27 +0800

    完善使用pinctl和gpio子系统的led灯驱动编写

--- 5_gpioled/gpioled.c
+++ 5_gpioled/gpioled.c
@@ -11,6 +11,8 @@
 #include <linux/of_address.h>
 #include <linux/of_irq.h>
 #include <linux/types.h>
+#include <linux/gpio.h>
+#include <linux/of_gpio.h>
 
 /* cdev 系列操作是“内核注册”，后面做的 class 和 device 是“应用层可见性创建”。内核注册让驱动能用，可见性创建让应用程序能找到它*/
 
@@ -27,55 +29,49 @@ struct gpioled_dev
     struct cdev cdev;
     struct class *class;
     struct device *device;
+    struct device_node* nd;
     int major;
     int minor;
+    int led_gpio;
+
 };
 
 struct gpioled_dev gpioled;
 
 static int led_open(struct inode *inode, struct file *filp)
 {
-    // filp->private_data = &led;
+    filp->private_data = &gpioled;
     return 0;
 }
 
 static int led_release(struct inode *inode, struct file *filp)
 {
-    // struct led_dev *dev = (struct led_dev *)filp->private_data;
+    struct gpioled_dev *dev = (struct led_dev *)filp->private_data;
     return 0;
 }
 
-// led 开关
-static void led_switch(u8 state)
-{
-    // u32 val = 0;
-    // if (state == LED_ON)
-    // {
-    //     val = readl(GPIO1_DR);
-    //     val &= ~(1 << 3); // 清零bit3, 打开led
-    //     writel(val, GPIO1_DR);
-    // }
-    // else if (state == LED_OFF)
-    // {
-    //     val = readl(GPIO1_DR);
-    //     val |= (1 << 3);
-    //     writel(val, GPIO1_DR);
-    // }
-}
 
 static ssize_t led_write(struct file *filp, const char __user *buf,
                             size_t count, loff_t *ppos)
 {
-    // unsigned char databuf[1];
-    // unsigned long ret = 0;
-    // ret = copy_from_user(databuf, buf, count);
-    // if (ret < 0)
-    // {
-    //     printk("kernel copy_from_user failed");
-    //     return -EFAULT;
-    // }
-    // /*判断开灯、关灯*/
-    // led_switch(databuf[0]);
+    unsigned char databuf[1];
+    unsigned long ret = 0;
+    struct gpioled_dev *dev = filp->private_data;
+    ret = copy_from_user(databuf, buf, count);
+    if (ret < 0)
+    {
+        printk("kernel copy_from_user failed");
+        return -EFAULT;
+    }
+    /*判断开灯、关灯*/
+    if(databuf[0] == LED_ON)
+    {
+        gpio_set_value(dev->led_gpio, 0);
+    }
+    else if(databuf[0] == LED_OFF)
+    {
+        gpio_set_value(dev->led_gpio, 1);
+    }
     return 0;
 }
 /*操作集*/
@@ -89,6 +85,7 @@ static const struct file_operations led_fops = {
 /*入口*/
 static int __init led_init(void)
 {
+    int ret = 0;
     /*注册字符驱动*/
     gpioled.major = 0;
     if (gpioled.major)
@@ -123,15 +120,56 @@ static int __init led_init(void)
     if (IS_ERR(gpioled.device))
     {
         return PTR_ERR(gpioled.device);
-
-        
     }
+
+    /*获取设备结点*/
+    gpioled.nd = of_find_node_by_path("/gpioled");
+    if(gpioled.nd == NULL)
+    {
+        ret = -EINVAL;
+        goto fail_findnode;
+    }
+
+    /*获取led对应的GPIO*/
+    gpioled.led_gpio = of_get_named_gpio(gpioled.nd, "led-gpios", 0);
+    if(gpioled.led_gpio < 0)
+    {
+        printk("cannot find led_gpio\r\n");
+        ret = -EINVAL;
+        goto fail_findgpio;
+    }
+    /*申请gpio*/
+    ret = gpio_request(gpioled.led_gpio,  "led_gpio");
+    if(ret)
+    {
+        printk("failed led gpio request\r\n");
+        ret = -EINVAL;
+        goto fail_gpiorequest;
+    }
+    /*使用GPIO, 设置为输出*/
+    ret = gpio_direction_output(gpioled.led_gpio, 1);
+    if(ret)
+    {
+        goto fail_setoutput;
+    }
+
+    /*设置输出低电平, 开灯*/
+    gpio_set_value(gpioled.led_gpio, 0);
+
     return 0;
+fail_setoutput:
+    gpio_free(gpioled.led_gpio);
+fail_gpiorequest:
+fail_findgpio:
+fail_findnode:
+    return ret;
 }
 /*出口*/
 
 static void __exit led_exit(void)
 {
+    /*关灯*/
+    gpio_set_value(gpioled.led_gpio, 1);
     /*注销设备*/
     cdev_del(&gpioled.cdev);
     unregister_chrdev_region(gpioled.devid, GPIOLED_CNT);
@@ -140,6 +178,9 @@ static void __exit led_exit(void)
 
     /*销毁类*/
     class_destroy(gpioled.class);
+
+    /*释放gpio*/
+    gpio_free(gpioled.led_gpio);
 }
 
 module_init(led_init);
